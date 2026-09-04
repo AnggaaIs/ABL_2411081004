2d02f463e67fbd5758bc7e92bf4a49825bde6986
AnggaaIs <servantangga@gmail.com>
2026-09-04 11:03:43 +0700

    menambah entitas produk

--- src/main/java/com/angga/produk/controller/ProdukController.java
+++ src/main/java/com/angga/produk/controller/ProdukController.java
@@ -1,25 +1,42 @@
 package com.angga.produk.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.angga.produk.entity.Produk;
 
 @RestController
 @RequestMapping("/api/produk")
 public class ProdukController {
   @GetMapping
-  public String ping() {
-    return "ping";
+  public List<Produk> getAllProduk() {
+    List<Produk> produks = List.of(
+        new Produk(1, "Mouse", 100000, "Merek 1"),
+        new Produk(2, "Mouse", 200000, "Merek 2"));
+
+    return produks;
+
   }
 
   @GetMapping("/{id}")
-  public String getProdukById(@PathVariable("id") int id) {
+  public ResponseEntity<Produk> getProdukById(@PathVariable("id") int id) {
     if (id == 1) {
-      return "Produk 1";
+      return ResponseEntity.ok(new Produk(1, "Mouse", 10000, "Merek 1"));
     }
 
-    return "Produk tidak ditemukan";
+    return null;
 
+  }
+
+  @PostMapping
+  public ResponseEntity<Produk> createProduk(@RequestBody Produk produk) {
+    return ResponseEntity.ok(produk);
   }
 }
